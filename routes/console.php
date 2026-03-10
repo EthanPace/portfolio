a5e4daf4635daf45bb2fb9125ba071abdf67d324
Ethan <ethanpace212@gmail.com>
2026-03-11 07:20:02 +1000

    chang to cron timing

--- routes/console.php
+++ routes/console.php
@@ -5,8 +5,5 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
-
-Schedule::job(new FetchStats)->everyMinutes((int) config('services.stats.cache_hours') * 60);
+$hours = config('services.stats.cache_hours');
+Schedule::job(new FetchStats)->cron("0 */{$hours} * * *");
